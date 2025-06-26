7dded20dcebb9df9a4efce8a0ca2e91ac218006d
Javy1996 <javytorovc1@gmail.com>
2025-06-26 09:20:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ else:
 # Cargar clave API
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-Settings.llm = OpenAI(api_key=openai.api_key, model="gpt-3.5-turbo-0125", temperature=0.9)
+Settings.llm = OpenAI(api_key=openai.api_key, model="gpt-3.5-turbo", temperature=0.9)
 
 # Crear o cargar índice
 @st.cache_resource
